6584bb01914f9f5eaffc56ed78ac9ce052266707
David Jay <davidjay@samsungaccelerator.com>
2015-05-03 19:59:50 -0500

    Basic force graph, getting nodes to appear.

--- app/directives.js
+++ app/directives.js
@@ -39,12 +39,11 @@ angular.module('relvis.directives', [])
 
 						scope.$watch('$parent.data', 
 							function(newVals, oldVals) { 
-								console.log("Registered data change:" + newVals);
 								return scope.render(newVals); 
 							}, true);
 
 						scope.render = function(data) { 
-							console.log("Rendering:" + data);
+
 							// remove all previous items before render 
 							svg.selectAll('*').remove();
 
@@ -55,35 +54,83 @@ angular.module('relvis.directives', [])
 							// setup variables 
 							var width = d3.select(element[0]).node().offsetWidth - margin, 
 							// calculate the height 
-							height = scope.$parent.data.length * (barHeight + barPadding), 
+							height = width, 
 							// Use the category20() scale function for multicolor support 
 							color = d3.scale.category20(), 
-							// our xScale 
-							xScale = d3.scale.linear() 
-								.domain([0, d3.max(data, function(d) { 
-									return d.score; 
-								})]) 
-								.range([0, width]);
+
 							// set the height based on the calculations above 
+							force = d3.layout.force() 
+								.charge(-120) 
+								.linkDistance(30) 
+								.size([width, height])
+								.nodes(scope.$parent.nodes)
+								.links(scope.$parent.lines)
+								.start();
+
 							svg.attr('height', height);
+							svg.attr('width', width);
+
+							var link = svg.selectAll(".link") 
+								.data(scope.$parent.lines) 
+								.enter()
+								.append("line") 
+								.attr("class", "link") 
+								.style("stroke-width", function(d) { 
+									return Math.sqrt(d.value); 
+								});
+
+							var node = svg.selectAll(".node") 
+								.data(scope.$parent.nodes) 
+								.enter()
+									.append("circle") 
+									.attr("class", "node") 
+									.attr("r", 5) 
+									.style("fill", function(d) { 
+										return color(d.group); 
+									}) 
+									.call(force.drag);
+
+							force.on("tick", 
+								function() { 
+									link.attr("x1", function(d) { 
+										return d.source.x; 
+									}) 
+									.attr("y1", function(d) { 
+										return d.source.y; 
+									}) 
+									.attr("x2", function(d) { 
+										return d.target.x; 
+									}) 
+									.attr("y2", function(d) { 
+										return d.target.y; 
+									}); 
+
+									node.attr("cx", function(d) { 
+										return d.x; 
+									}) 
+									.attr("cy", function(d) { 
+										return d.y; 
+									}); 
+								});
+
 							//create the rectangles for the bar chart 
-							svg.selectAll('rect') 
-								.data(data).enter() 
-									.append('rect') 
-									.attr('height', barHeight) 
-									.attr('width', 140) 
-									.attr('x', Math.round(margin/2)) 
-									.attr('y', function(d,i) { 
-										return i * (barHeight + barPadding); 
-									}) 
-									.attr('fill', function(d) { 
-										return color(d.score); 
-									}) 
-									.transition() 
-										.duration(1000) 
-										.attr('width', function(d) { 
-											return xScale(d.score); 
-										});
+							// svg.selectAll('rect') 
+							// 	.data(data).enter() 
+							// 		.append('rect') 
+							// 		.attr('height', barHeight) 
+							// 		.attr('width', 140) 
+							// 		.attr('x', Math.round(margin/2)) 
+							// 		.attr('y', function(d,i) { 
+							// 			return i * (barHeight + barPadding); 
+							// 		}) 
+							// 		.attr('fill', function(d) { 
+							// 			return color(d.score); 
+							// 		}) 
+							// 		.transition() 
+							// 			.duration(1000) 
+							// 			.attr('width', function(d) { 
+							// 				return xScale(d.score); 
+							// 			});
 						};
 				} 
 			}
